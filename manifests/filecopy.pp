1af059064d3ce2f70caeeb709e2c7f5e2fe0f30d
Ravi Kumar Nayak <ravinayak19@gmail.com>
2021-12-19 07:33:36 +0530

    Replacement of /etc with /codetestfiles

--- manifests/filecopy.pp
+++ manifests/filecopy.pp
@@ -1,10 +1,10 @@
 class realizeres2::filecopy{
   include virtualres
-  file { '/etc/filecopyrealizeres1':
+  file { '/codetestfiles/filecopyrealizeres1':
     ensure => file,
     owner  => 'neo5',
     group  => 'neo5',
     mode   => '0755',
-    source => '/etc/virtualexecfile.txt',
+    source => '/codetestfiles/virtualexecfile.txt',
   }
 }
